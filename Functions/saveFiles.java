6f06e2ce8c451d4910d3bfb806da9fecf488e462
Mohamed Taha <mohamedtahahelmy@gmail.com>
2022-06-07 09:33:50 +0200

    saved posts function is completed, I will then make the search function

--- Functions/saveFiles.java
+++ Functions/saveFiles.java
@@ -1,4 +1,25 @@
 package Functions;
 
+import java.io.*;
+import java.time.LocalDateTime;
+
+
 public class saveFiles {
+    public static void addMainPost(String postID, LocalDateTime postDate, String postContent) throws IOException {
+       // FileWriter writer = new FileWriter(String.valueOf(new FileOutputStream("mainPosts.txt")));
+        PrintWriter writer = new PrintWriter(new FileOutputStream("mainPosts.txt", true));
+        writer.println(postID);
+        writer.println(postDate);
+        writer.println(postContent);
+        writer.close();
+    }
+    public static void addReplyPost(String postID, String mainPostID ,LocalDateTime postDate, String postContent) throws IOException {
+        // FileWriter writer = new FileWriter(String.valueOf(new FileOutputStream("mainPosts.txt")));
+        PrintWriter writer = new PrintWriter(new FileOutputStream("replyPosts.txt", true));
+        writer.println(postID);
+        writer.println(mainPostID);
+        writer.println(postDate);
+        writer.println(postContent);
+        writer.close();
+    }
 }
