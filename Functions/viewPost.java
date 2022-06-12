7bd42a9ae1fc13678549b33ab8f0c53498c86266
CrissWei <92811234+CrissWei@users.noreply.github.com>
2022-06-13 01:16:03 +0800

    add ReplyPost-- Cris

--- Functions/viewPost.java
+++ Functions/viewPost.java
@@ -202,21 +202,21 @@ public class viewPost {
         PostTree tree = new PostTree();
         PostQueue queue = new PostQueue();
         try {
-            queue.addPost(tree, new Post(1234, "hi world"));
+            queue.addPostQueue(tree, new Post(1234, "hi world"));
             Post parent = tree.findPost(1234);
-            queue.addPost(tree, new Post(1235, parent, "hi world 2"));
-            queue.addPost(tree, new Post(4, "i am ok"));
-            queue.addPost(tree, new Post(5, "i am ok"));
+            queue.addPostQueue(tree, new Post(1235, parent, "hi world 2"));
+            queue.addPostQueue(tree, new Post(4, "i am ok"));
+            queue.addPostQueue(tree, new Post(5, "i am ok"));
             Post answerToEverything = new Post(42, "answer to everything");
-            queue.addPost(tree, answerToEverything);
-            queue.addPost(tree, new Post(43, answerToEverything, "really? \"It might just be fake...\""));
-            queue.addPost(tree, new Post(44, answerToEverything, "It's fake!\nDon't believe him!"));
-            queue.addPost(tree, new Post(45, answerToEverything, "hmmmm...."));
+            queue.addPostQueue(tree, answerToEverything);
+            queue.addPostQueue(tree, new Post(43, answerToEverything, "really? \"It might just be fake...\""));
+            queue.addPostQueue(tree, new Post(44, answerToEverything, "It's fake!\nDon't believe him!"));
+            queue.addPostQueue(tree, new Post(45, answerToEverything, "hmmmm...."));
             Post reply1 = tree.findPost(45);
-            queue.addPost(tree, new Post(46, reply1, "What you think #45?"));
+            queue.addPostQueue(tree, new Post(46, reply1, "What you think #45?"));
             reply1 = tree.findPost(46);
-            queue.addPost(tree, new Post(47, reply1, "Maybe."));
-            queue.addPost(tree, new Post(48, reply1, "Maybe too."));
+            queue.addPostQueue(tree, new Post(47, reply1, "Maybe."));
+            queue.addPostQueue(tree, new Post(48, reply1, "Maybe too."));
         } catch (InterruptedException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
