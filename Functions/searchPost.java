e131d09858aa9fe0d4aaeaabde2fa527aa7dd716
CrissWei <92811234+CrissWei@users.noreply.github.com>
2022-06-15 01:29:23 +0800

    need edit searchPost and password

--- Functions/searchPost.java
+++ Functions/searchPost.java
@@ -1,4 +1,14 @@
 package Functions;
 
 public class searchPost {
+    public boolean isEmpty(){
+        // codes
+
+        return false;
+    }
+    public String getKeyWord(String keyWord){
+        //search all post which including the keyWord
+
+        return null;
+    }
 }
